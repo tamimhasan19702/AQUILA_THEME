25b5bbd68e87633011e47de8dafce6fed4191fdf
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-05 02:01:38 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+
+/**
+ * Main template file
+ * 
+ * @package themeDev
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress</title>
+</head>
+
+<body>
+    <h1>
+        wordpress
+    </h1>
+</body>
+
+</html>
