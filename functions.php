4f80ffc504b7338cc52abc8ca17c3a2b81dc5b8f
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-06 18:52:36 +0600

    update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,10 @@
  * @package Arrow
  */
 
-  
+  function arrow_enqueue_scripts(){
+      wp_enqueue_style('arrow-style', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), "all" );
+  }
 
+  add_action('wp_enqueue_scripts', 'arrow_enqueue_scripts');
 
 ?>
