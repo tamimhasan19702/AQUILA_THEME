76b12d7d325b500f9df5b7d5ef830276f845c128
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-08 13:01:59 +0600

    update functions.php

--- functions.php
+++ functions.php
@@ -8,16 +8,48 @@
 
   function arrow_enqueue_scripts(){
     
-    //   wp_enqueue_style('arrow-style', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), "all" );
+    //Register Styles
+   wp_register_style(
+    'style-css',
+    get_stylesheet_uri(),
+    [],
+    filemtime(get_template_directory() . '/style.css'),
+    'all'
+    );
+   wp_register_style(
+    'bootstap-css',
+    get_stylesheet_uri().'/assets/src/library/css/bootstrap.min.css',
+    [],
+    false,
+    'all'
+    );
+
+    //Register Scripts
+    wp_register_script(
+      'main-js',
+      get_template_directory_uri(),
+      '/assets/main.js',
+      [],
+      filemtime(get_template_directory() . '/assets/main.js'),
+      true
+    );
+    wp_register_script(
+      'bootstarp-js',
+      get_template_directory_uri(),
+      '/assets/src/library/js/bootstrap.min.js',
+      ['jquery'],
+      false,
+      true
+    );
+
+    //Enqueue Styles
+    wp_enqueue_style('style-css');
+    wp_enqueue_style('bootstap-css');
+
+    //Enqueue Scripts
+    wp_enqueue_script('main-js');
+    wp_enqueue_script('bootstarp-js');
     
-    wp_register_style( "arrow-style", get_template_directory_uri() . "/style.css", [], filemtime( get_template_directory() . '/style.css' ), "all" );
-
-    wp_enqueue_style( "arrow-style" );
-    
-    wp_enqueue_script( "arrow-script", get_template_directory_uri() . "/assets/main.js", [], filemtime( get_template_directory() . '/assets/main.js' ), true );
-
-
-
   }
 
   add_action('wp_enqueue_scripts', 'arrow_enqueue_scripts');
