0b3b2e92b561f0cc952794bd3775847bac2917d7
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-08 19:24:11 +0600

    bootstrap stylesheet finally working in the frontend

--- functions.php
+++ functions.php
@@ -6,52 +6,50 @@
  * @package Arrow
  */
 
-  function arrow_enqueue_scripts(){
-    
-    //Register Styles
-   wp_register_style(
-    'style-css',
+function arrow_enqueue_scripts(){
+  
+  // Register Styles
+  wp_register_style(
+    'style',
     get_stylesheet_uri(),
     [],
     filemtime(get_template_directory() . '/style.css'),
     'all'
-    );
-   wp_register_style(
-    'bootstap-css',
-    get_stylesheet_uri().'/assets/src/library/css/bootstrap.min.css',
+  );
+  wp_register_style(
+    'bootstrap',
+    get_template_directory_uri().'/assets/src/library/css/bootstrap.min.css',
     [],
     false,
     'all'
-    );
+  );
 
-    //Register Scripts
-    wp_register_script(
-      'main-js',
-      get_template_directory_uri(),
-      '/assets/main.js',
-      [],
-      filemtime(get_template_directory() . '/assets/main.js'),
-      true
-    );
-    wp_register_script(
-      'bootstarp-js',
-      get_template_directory_uri(),
-      '/assets/src/library/js/bootstrap.min.js',
-      ['jquery'],
-      false,
-      true
-    );
+  // Register Scripts
+  wp_register_script(
+    'main',
+    get_template_directory_uri().'/assets/main.js',
+    [],
+    filemtime(get_template_directory() . '/assets/main.js'),
+    true
+  );
+  wp_register_script(
+    'bootstarp',
+    get_template_directory_uri().'/assets/src/library/js/bootstrap.min.js',
+    ['jquery'],
+    false,
+    true
+  );
 
-    //Enqueue Styles
-    wp_enqueue_style('style-css');
-    wp_enqueue_style('bootstap-css');
+  // Enqueue Styles
+  wp_enqueue_style('style');
+  wp_enqueue_style('bootstrap');
 
-    //Enqueue Scripts
-    wp_enqueue_script('main-js');
-    wp_enqueue_script('bootstarp-js');
-    
-  }
+  // Enqueue Scripts
+  wp_enqueue_script('main');
+  wp_enqueue_script('bootstarp');
+  
+}
 
-  add_action('wp_enqueue_scripts', 'arrow_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'arrow_enqueue_scripts');
 
 ?>
