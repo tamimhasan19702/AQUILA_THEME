963ffc6655e7bb73f2cf446ea0a11498e551e110
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-16 01:46:30 +0600

    update class-arrow-theme.php and class-assets.php

--- inc/classes/class-arrow-theme.php
+++ inc/classes/class-arrow-theme.php
@@ -16,61 +16,29 @@
     protected function __construct(){
 
         //load class
+        Assets::get_instance();
+
         $this->setup_hooks();
     }
 
     protected function setup_hooks(){
-        //actions and filters
-        add_action('wp_enqueue_scripts', [$this, 'register_styles']); 
-        add_action('wp_enqueue_scripts', [$this, 'register_scripts']); 
+    /**
+     * Actions
+     */
+    add_action('after_setup_theme', [$this, 'setup_theme']);
+    }
+
+    public function setup_theme(){
+        add_theme_support('title-tag');
+
+        add_theme_support('custom-logo', [
+            'header-text' => ['site-title','site-description'],
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width' => true,
+        ]);
     }
     
-    public function register_styles(){
-  // Register Styles
-  wp_register_style(
-    'style',
-    get_stylesheet_uri(),
-    [],
-    filemtime(ARROW_DIR_PATH . '/style.css'),
-    'all'
-  );
-  wp_register_style(
-    'bootstrap',
-    ARROW_DIR_URI .'/assets/src/library/css/bootstrap.min.css',
-    [],
-    false,
-    'all'
-  );
-
-  
-  // Enqueue Styles
-  wp_enqueue_style('style');
-  wp_enqueue_style('bootstrap');
-    }
-
-    public function register_scripts(){
-        
-  // Register Scripts
-  wp_register_script(
-    'main',
-    ARROW_DIR_URI .'/assets/main.js',
-    [],
-    filemtime(ARROW_DIR_PATH . '/assets/main.js'),
-    true
-  );
-  wp_register_script(
-    'bootstarp',
-    ARROW_DIR_URI .'/assets/src/library/js/bootstrap.min.js',
-    ['jquery'],
-    false,
-    true
-  );
-
-
-  // Enqueue Scripts
-  wp_enqueue_script('main');
-  wp_enqueue_script('bootstarp');
-  
-    }
 
  }
