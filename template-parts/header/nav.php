19d4f95107edadc57feacf68c879ee706d1c6717
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-17 23:39:56 +0600

    update nav.php

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 /**
  * Header Navigation template
- * 
+ *
  * @package Arrow
  */
 
@@ -9,17 +9,15 @@ $menu_class = \ARROW_THEME\Inc\Menus::get_instance();
 $header_menu_id = $menu_class->get_menu_id('arrow-header-menu');
 
 $header_menus = wp_get_nav_menu_items($header_menu_id);
-
-
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
     <?php
-    if(function_exists('the_custom_logo')){
+    if (function_exists('the_custom_logo')) {
         the_custom_logo();
     }
-     ?>
+    ?>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,49 +26,65 @@ $header_menus = wp_get_nav_menu_items($header_menu_id);
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        <?php 
-         if( !empty($header_menus) && is_array($header_menus)){ ?>
+        <?php if (!empty($header_menus) && is_array($header_menus)): ?>
         <ul class="navbar-nav mr-auto">
 
-            <?php 
-        foreach($header_menus as $menu_item){
-            if(! $menu_item-> menu_item_parent){
+            <?php foreach ($header_menus as $header_menu_item): ?>
 
-                $child_menu_items = $menu_class->get_child_menu_items($header_menus, $menu_item->ID);
+            <?php
+                  
+                  if (!$header_menu_item->menu_item_parent) {
 
-                echo '<pre>';
-                print_r( $child_menu_items);
-                wp_die();
-            }}
-            ?>
+                       $child_menu_items = $menu_class->get_child_menu_items($header_menus, $header_menu_item->ID);
+                       $has_children = !empty($child_menu_items) && is_array($child_menu_items);
+
+                   ?>
+
+            <?php if (!$has_children): ?>
+
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+
+                <a class="nav-link" href="<?php echo esc_url($header_menu_item->url); ?>">
+                    <?php echo esc_html($header_menu_item->title); ?>
+                </a>
+
             </li>
 
+            <?php else: ?>
+
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+
+                <a class="nav-link dropdown-toggle" href="<?php echo esc_url($header_menu_item->url); ?>"
+                    id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <?php echo esc_html($header_menu_item->title); ?>
                 </a>
+
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <?php foreach ($child_menu_items as $child_menu_item): ?>
+
+                    <a href="<?php echo esc_url($child_menu_item->url); ?>" class="dropdown-item">
+                        <?php echo esc_html($child_menu_item->title); ?>
+                    </a>
+
+                    <?php endforeach; ?>
+
                 </div>
             </li>
 
-            }
-            ?>
+            <?php endif; ?>
+
+            <?php } ?>
+
+            <?php endforeach; ?>
 
         </ul>
-        <?php
-        }
-        ?>
+
+        <?php endif; ?>
 
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+
     </div>
 </nav>
