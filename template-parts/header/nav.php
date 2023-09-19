6f005bddbed697c156d1ec664690ced3779d0c9d
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-19 20:39:04 +0600

    delete 1 file and update 2 files

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -17,29 +17,30 @@ $header_menus = wp_get_nav_menu_items($header_menu_id);
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-    <!-- this is for custom logo -->
-    <?php
+    <div class="container">
+        <!-- this is for custom logo -->
+        <?php
     if (function_exists('the_custom_logo')) {
         the_custom_logo();
     }
     ?>
 
-    <!-- this is a toggler button -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+        <!-- this is a toggler button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        <?php 
+            <?php 
      
        if(!empty($header_menus) && is_array($header_menus)){
         ?>
 
-        <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav mr-auto">
 
-            <?php 
+                <?php 
         foreach($header_menus as $menu_item){
             if( ! $menu_item->menu_item_parent){
 
@@ -50,68 +51,71 @@ $header_menus = wp_get_nav_menu_items($header_menu_id);
                 if(! $has_children){
                 ?>
 
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo esc_url($menu_item->url); ?>">
-                    <?php echo esc_html($menu_item->title); ?>
-                </a>
-            </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo esc_url($menu_item->url); ?>">
+                        <?php echo esc_html($menu_item->title); ?>
+                    </a>
+                </li>
 
-            <?php
+                <?php
             
                 }else{
                 ?>
 
-            <li class="nav-item dropdown">
+                <li class="nav-item dropdown">
 
-                <a class="nav-link dropdown-toggle" href="<?php echo esc_url($menu_item->url); ?>" id="navbarDropdown"
-                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="<?php echo esc_url($menu_item->url); ?>"
+                        id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
 
-                    <?php echo esc_html($menu_item->title); ?>
+                        <?php echo esc_html($menu_item->title); ?>
 
-                </a>
+                    </a>
 
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 
-                    <?php 
+                        <?php 
                     foreach($child_menu_items as $child_menu_item){
                     ?>
 
-                    <a class="dropdown-item" href="<?php echo esc_url($child_menu_item->url) ?>">
-                        <?php echo esc_html($child_menu_item->title) ?>
-                    </a>
+                        <a class="dropdown-item" href="<?php echo esc_url($child_menu_item->url) ?>">
+                            <?php echo esc_html($child_menu_item->title) ?>
+                        </a>
 
-                    <?php
+                        <?php
                     }
                     ?>
 
-                </div>
+                    </div>
 
-            </li>
+                </li>
 
-            <?php
+                <?php
                 }
                 
                 ?>
 
-            <?php 
+                <?php 
                
             }
 
             }
             ?>
 
-        </ul>
+            </ul>
 
-        <?php
+            <?php
         }
 
         ?>
 
-        <!--search form -->
-        <form class="form-inline my-2 my-lg-0">
-            <input type="search" class="form-control mr-sm-2" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+            <!--search form -->
+            <form class="form-inline my-2 my-lg-0">
+                <input type="search" class="form-control mr-sm-2" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
 
+        </div>
     </div>
+
 </nav>
