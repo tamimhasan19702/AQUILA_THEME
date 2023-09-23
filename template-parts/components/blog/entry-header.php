d87a77882b6470193e54480b868d03baed745877
tamimhasan19702 <tamimhasancu@gmail.com>
2023-09-23 20:24:44 +0600

    update template-tags.php and entry-header.php

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -15,12 +15,21 @@ $has_post_thumbnail = get_the_post_thumbnail($the_post_id);
     //Featured image
     if ($has_post_thumbnail) {
         ?>
-    <div class="entry-image mb-3">
-        <a href="<?php echo esc_url(get_permalink()); ?>">
-
-        </a>
-    </div>
-    <?php
+        <div class="entry-image mb-3">
+            <a href="<?php echo esc_url(get_permalink()); ?>">
+                <?php
+                the_post_custom_thumbnail(
+                    $the_post_id,
+                    'featured-large',
+                    [
+                        'sizes' => '(max-width: 590px) 590px, 425px',
+                        'class' => 'attachment-featured-large size-featured-image'
+                    ]
+                )
+                    ?>
+            </a>
+        </div>
+        <?php
     }
     ?>
 </header>
