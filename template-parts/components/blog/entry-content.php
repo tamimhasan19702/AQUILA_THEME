e5a8d4984080fdeac1d2480caa44fea8c6867ff0
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-08 21:23:33 +0600

    blog excerpt custom function created

---
+++ template-parts/components/blog/entry-content.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Template part for displaying post content
+ * 
+ * To be used inside the Wordpress Loop
+ * @package arrow
+ */
+?>
+
+<div class="entry-content">
+    <?php
+    if (is_single()) {
+        the_content(
+            sprintf(
+                wp_kses(
+                    __('Continue reading %s <span class="meta-nav">&rarr;</span>', 'arrow'),
+                    ['span' => ['class' => []]]
+                ),
+                the_title('<span class="screen-reader-text">"', '"</span>', false)
+            )
+        );
+    } else {
+        arrow_the_excerpt();
+    }
+    ?>
+</div>
