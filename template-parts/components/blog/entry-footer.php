4dcaab2c73e5a99490504bc046d34dcfcbf35b31
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-14 23:24:40 +0600

    update entry-footer.php

--- template-parts/components/blog/entry-footer.php
+++ template-parts/components/blog/entry-footer.php
@@ -1,7 +1,20 @@
 <?php
 /**
  * Content template
- * 
+ *
+ * To be used insdie the wordpress loop
+ *  
  * @package arrow
  */
+
+
+$the_post_id = get_the_ID();
+$article_terms = wp_get_post_terms($the_post_id, ['category', 'post_tag']);
+
 ?>
+
+<div class="entry-footer mt-4">
+    <?php
+
+    ?>
+</div>
